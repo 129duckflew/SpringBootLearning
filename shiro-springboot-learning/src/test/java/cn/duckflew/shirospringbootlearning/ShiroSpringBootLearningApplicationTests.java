7c52b6931b8b8ac493859799faa7a128a8162350
129duckflew <129duckflew@gmail.com>
2021-07-09 04:29:23 +0800

    优化项目结构 删除无用代码

---
+++ shiro-springboot-learning/src/test/java/cn/duckflew/shirospringbootlearning/ShiroSpringBootLearningApplicationTests.java
@@ -0,0 +1,15 @@
+package cn.duckflew.shirospringbootlearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiroSpringBootLearningApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
