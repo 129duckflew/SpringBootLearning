ba25f687f04e828fb033141ec1ed140551d8e7c6
duckflew <129duckflew@gmail.com>
2021-10-06 00:00:57 +0800

    精简代码

--- SpringBootRedisCache/src/main/java/cn/duckflew/entity/Book.java
+++ SpringBootRedisCache/src/main/java/cn/duckflew/entity/Book.java
@@ -1,11 +1,14 @@
 package cn.duckflew.entity;
 
 
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
@@ -18,4 +21,7 @@ public class Book implements Serializable
 
     private String author;
 
+    @TableField("createTime")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createTime;
 }
